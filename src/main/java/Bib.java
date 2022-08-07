9287f1037cefdcac104c19e076ae7c53aa8e675e
hlibb <Astashev01@gmail.com>
2022-08-07 14:08:56 +0200

    init

--- src/main/java/Bib.java
+++ src/main/java/Bib.java
@@ -1,17 +1,23 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.databind.type.ArrayType;
 import com.fasterxml.jackson.databind.type.CollectionType;
 
-import java.awt.*;
+import java.io.File;
+import java.util.List;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Bib {
-    String path = "src/main/java/Bibliothek.json";
+    private final File path = new File("Bibliothek.json");
     ArrayList<Book> bib = new ArrayList<>();
+
+    public void pullFile() throws IOException {
+        ObjectMapper jsonMapper = new JsonMapper();
+        CollectionType javaType = jsonMapper.getTypeFactory().constructCollectionType(bib.getClass(), Book.class);
+        bib = jsonMapper.readValue(path, javaType);
+    }
 
     public void add(Book book) {
         bib.add(book);
@@ -25,17 +31,12 @@ public class Bib {
         String catalog = "+++++++++++ CATALOG +++++++++++";
         System.out.println(catalog);
         for (Book book : bib) {
-            System.out.printf("%s-20, %s-20, %d-4", book.getName(), book.getAuthor(), book.getYear());
+            System.out.printf("%15s, %10s, %4d\n", book.getName(), book.getAuthor(), book.getYear());
         }
     }
 
-    public Bib pullFile() throws IOException {
+    public void pushFile() throws IOException {
         ObjectMapper jsonMapper = new JsonMapper();
-        CollectionType javaType = jsonMapper.getTypeFactory().constructCollectionType(Book[].class, Book.class);
-        Bib bib1 = jsonMapper.readValue(path, Bib.class);
-    }
-
-    public void pushFile() {
-
+        jsonMapper.writeValue(path, bib);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,26 +6,48 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Bib bib = new Bib();
+        bib.add(new Book("World Of Warcraft", "Blizzard", 1998));
+        bib.pullFile();
+
+        String menu = "____________________________\n" +
+                "1- Add\n" +
+                "2- Delete\n" +
+                "3- Catalog\n" +
+                "-1- Exit\n" +
+                "____________________________";
 
         Scanner sc = new Scanner(System.in);
-        File bibliothek = new File("src/main/java/Bibliothek.json");
+        System.out.println(menu);
         while (!sc.hasNext("-1")) {
             switch (sc.nextLine()) {
-                case ("1"):
+                case ("1"): {
                     System.out.println("Name: ");
-                    String name = new String(sc.nextLine());
+                    String name = sc.nextLine();
                     System.out.println("Author: ");
-                    String author = new String(sc.nextLine());
+                    String author = sc.nextLine();
                     System.out.println("Year: ");
                     int year = sc.nextInt();
-                case ("2"):
+                    System.out.println(menu);
+                    bib.add(new Book(name, author, year));
+                    break;
+                }
+                case ("2"): {
                     System.out.println("Enter the name of the book to delete: ");
                     String toDelete = sc.nextLine();
-                    for (Book book : bib.bib) {
-                        if (book.getName().equals(toDelete)) bib.bib.remove(toDelete);
-                    }
+                    bib.bib.removeIf(book -> book.getName().equals(toDelete));
+                    System.out.println("Removed");
+                    System.out.println(menu);
+                    break;
+                }
+                case ("3"): {
+                    bib.show();
+                    System.out.println(menu);
+                    break;
+                }
+                default:
+                    System.err.println("Unknown error");
             }
-
         }
+        bib.pushFile();
     }
 }
